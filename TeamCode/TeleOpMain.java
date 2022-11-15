e7004cf26a2be205549ba1cddabacf6ceb338c89
Nicholas Sullivan-Friedman <rpgcubed@gmail.com>
2022-11-15 14:48:48 -0800

    feat!: clean up unneeded DANGER

--- TeamCode/TeleOpMain.java
+++ TeamCode/TeleOpMain.java
@@ -21,117 +21,52 @@ public class TeleOpMain extends BaseOpMode {
 
         // Wait for the driver to press the Start button on the Driver Hub
         waitForStart();
+
+        // Start position/acceleration tracking for IMU
         imu.startAccelerationIntegration(new Position(), new Velocity(), 10);
 
-        // Loop until the robot is stopped
-        long lastTime = System.nanoTime() / 1000000;
         while (opModeIsActive()) {
-            long time = System.nanoTime() / 1000000;
-            int deltaTime = (int) (time - lastTime);
-            lastTime = time;
 
             // The left joystick controls the translation of the robot,
             // while the right joystick controls the rotation.
 
+            float inputSensitivity = 1f;
+            if (gamepad1.left_bumper) {
+                inputSensitivity = 0.1f;
+            }
+            float maxDrivetrainRotationsPerSecond = 300f;
+
             // Get the gamepad inputs
             // The y axis is vertical and "backwards",
             // so we negate it to make it positive when going forward
-            // ^ +y
-            // |
-            // <- ->
-            // -x | +x
-            // v -y
-            float sensitivity = 1f;
-            float rotationsPerSecond = 300f;
-            if (gamepad1.left_bumper) {
-                sensitivity = 1f;
-            }
-            float deltaY = -gamepad1.left_stick_y;
-            float deltaX = gamepad1.left_stick_x;
+            float deltaYInput = -gamepad1.left_stick_y;
+            float deltaXInput = gamepad1.left_stick_x;
+            float rotationInput = gamepad1.right_stick_x;
 
-            // Old rotation used as new rotation is bugged
-
-            float oldRotation = gamepad1.right_stick_x;
-
-            // Rotate by moving right stick left-right
-
-            float playerRotation = gamepad1.right_stick_x;
-            targetRotation -= playerRotation * ROTATION_SPEED;
-
-            // First, we need to split the translation vector into a direction and a
-            // magnitude.
-            // The direction is the direction to move
-            float direction = (float) Math.atan2(deltaY, deltaX);
+            // First, we need to split the translation vector
+            // into a direction and a magnitude.
+            // The direction is the direction to move in radians
+            float direction = (float) Math.atan2(deltaYInput, deltaXInput);
+            // The magnitude is how fast to move
+            float magnitude = (float) Math.sqrt(Math.pow(deltaXInput, 2) + Math.pow(deltaYInput, 2));
             telemetry.addData("Direction: %f", direction);
+            // The direction must be rotated
             if (direction > 0) {
                 direction = (float) Math.PI - direction;
             } else {
                 direction = -direction - (float) Math.PI;
             }
-            // The magnitude is how fast to move
-            float magnitude = (float) Math.sqrt(Math.pow(deltaX, 2) + Math.pow(deltaY, 2));
-            float speed = rotationsPerSecond
+            float maxDriveSpeed = maxDrivetrainRotationsPerSecond
                     * BasicMecanumDrive.DRIVE_TICKS_PER_ROTATION;
 
-            // Amend rotation to correct for drift
-            float currentRotation = imu.getAngularOrientation(AxesReference.INTRINSIC, AxesOrder.ZYX,
-                    AngleUnit.DEGREES).firstAngle;
-            float remappedTargetRotation = targetRotation + 180f;
-            float inRangeRemappedTargetRotation = (remappedTargetRotation % 360f + 360f) % 360f;
-            targetRotation = inRangeRemappedTargetRotation - 180f;
-            float rotationStep = 45f;
-            float steppedTargetRotation = ((float) Math.round(targetRotation / rotationStep) * rotationStep);
-            if (steppedTargetRotation == -180f)
-                steppedTargetRotation = -179f;
-            float rotationDelta = steppedTargetRotation - currentRotation;
-            if (rotationDelta > 180f)
-                rotationDelta -= 360f;
-            if (rotationDelta < -180f)
-                rotationDelta += 360f;
-            float finalRotationPower = rotationDelta / -3600f;
-            telemetry.addLine("Rotation");
-            telemetry.addData("Current: ", currentRotation);
-            telemetry.addData("Target: ", targetRotation);
-            telemetry.addData("Remapped: ", inRangeRemappedTargetRotation);
-            telemetry.addData("Stepped: ", steppedTargetRotation);
-            telemetry.addData("Final Power", finalRotationPower);
-
-            // TARGET POSITION CALCS
-
-            Position position = imu.getPosition();
-            telemetry.addData("Position: ", position);
-            Acceleration acceleration = imu.getAcceleration();
-            telemetry.addData("Acceleration: ", acceleration);
-            float xAccel = 0f;
-            if (Math.abs(acceleration.xAccel) > 0.5f) {
-                xAccel = (float) acceleration.xAccel;
-            }
-            ;
-            float yAccel = 0f; // Grabs raw Z due to orientation of hub
-            if (Math.abs(acceleration.zAccel) > 0.5f) {
-                yAccel = (float) acceleration.zAccel;
-            }
-            ;
-            currentDeltaX += xAccel * deltaTime / 1000;
-            currentDeltaY += yAccel * deltaTime / 1000;
-            currentX += currentDeltaX * deltaTime / 1000;
-            currentY += currentDeltaY * deltaTime / 1000;
-            telemetry.addData("Current Delta X: ", currentDeltaX);
-            telemetry.addData("Current Delta Y: ", currentDeltaY);
-            telemetry.addData("Current X: ", currentX);
-            telemetry.addData("Current Y: ", currentY);
-
-            // END TARGET POSITION CALCS
-
-            // TODO Explain these calculations
-            float leftFrontPower = magnitude * (float) Math.sin(direction + Math.PI / 4) + oldRotation;
-            float leftBackPower = magnitude * (float) Math.sin(direction - Math.PI / 4) + oldRotation;
-            float rightFrontPower = magnitude * (float) Math.sin(direction - Math.PI / 4) - oldRotation;
-            float rightBackPower = magnitude * (float) Math.sin(direction + Math.PI / 4) - oldRotation;
+            float leftFrontPower = magnitude * (float) Math.sin(direction + Math.PI / 4) + rotationInput;
+            float leftBackPower = magnitude * (float) Math.sin(direction - Math.PI / 4) + rotationInput;
+            float rightFrontPower = magnitude * (float) Math.sin(direction - Math.PI / 4) - rotationInput;
+            float rightBackPower = magnitude * (float) Math.sin(direction + Math.PI / 4) - rotationInput;
 
             // All of the values must be scaled to be within [-1,1]
             // First, find the highest value
-            float maxPower = sensitivity *
+            float maxPower = inputSensitivity *
                     Math.max(1f, Math.max(Math.abs(leftFrontPower), Math.max(Math.abs(rightBackPower),
                             Math.max(Math.abs(leftBackPower), Math.abs(rightBackPower)))));
 
@@ -141,11 +76,11 @@ public class TeleOpMain extends BaseOpMode {
             rightFrontPower = rightFrontPower / maxPower;
             rightBackPower = rightBackPower / maxPower;
 
-            // Set the motor power
-            leftFrontMotor.setVelocity(speed * leftFrontPower);
-            leftBackMotor.setVelocity(speed * leftBackPower);
-            rightFrontMotor.setVelocity(speed * rightFrontPower);
-            rightBackMotor.setVelocity(speed * rightBackPower);
+            // Set the motor velocity
+            leftFrontMotor.setVelocity(maxDriveSpeed * leftFrontPower);
+            leftBackMotor.setVelocity(maxDriveSpeed * leftBackPower);
+            rightFrontMotor.setVelocity(maxDriveSpeed * rightFrontPower);
+            rightBackMotor.setVelocity(maxDriveSpeed * rightBackPower);
 
             // START LIFT
             boolean motorUpInput = gamepad1.dpad_up;
